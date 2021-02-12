9416dceecf2cf6bcea6e3361ccbb7a5ba0aa0a0d
williamlidberg <78540233+williamlidberg@users.noreply.github.com>
2021-02-12 21:04:56 +0100

    Add files via upload

---
+++ LossFunction.py
@@ -0,0 +1,10 @@
+# Custom loss function
+smooth = 1.
+
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+def dice_coef_loss(y_true, y_pred):
+    return 1 - dice_coef(y_true, y_pred)
