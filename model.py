5ecdca187ac31f494eaa3fd785e1f4ab9d3cb6cc
williamlidberg <78540233+williamlidberg@users.noreply.github.com>
2021-02-12 21:02:55 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,67 +1,4 @@
-import numpy as np 
-import os
-import skimage.io as io
-import skimage.transform as trans
-import numpy as np
-from keras.models import *
-from keras.layers import *
-from keras.optimizers import *
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from keras import backend as keras
-import tensorflow as tf
-from keras import backend as K
-
-smooth = 1.
-
-def dice_coef(y_true, y_pred):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-
-
-def dice_coef_loss(y_true, y_pred):
-    return 1 - dice_coef(y_true, y_pred)
-
-# 
-def kappa_loss(y_true, y_pred, y_pow=2, eps=1e-10, N=2, bsize=16, name='kappa'):
-    """A continuous differentiable approximation of discrete kappa loss.
-        Args:
-            y_pred: 2D tensor or array, [batch_size, num_classes]
-            y_true: 2D tensor or array,[batch_size, num_classes]
-            y_pow: int,  e.g. y_pow=2
-            N: typically num_classes of the model
-            bsize: batch_size of the training or validation ops
-            eps: a float, prevents divide by zero
-            name: Optional scope/name for op_scope.
-        Returns:
-            A tensor with the kappa loss."""
-
-    with tf.name_scope(name):
-        y_true = tf.dtypes.cast(y_true, tf.float32)
-        repeat_op = tf.dtypes.cast(tf.tile(tf.reshape(tf.range(0, N), [N, 1]), [1, N]), tf.float32)
-        repeat_op_sq = tf.square((repeat_op - tf.transpose(repeat_op)))
-        weights = repeat_op_sq / tf.dtypes.cast((N - 1) ** 2, tf.float32)
-    
-        pred_ = y_pred ** y_pow
-        try:
-            pred_norm = pred_ / (eps + tf.reshape(tf.reduce_sum(pred_, 1), [-1, 1]))
-        except Exception:
-            pred_norm = pred_ / (eps + tf.reshape(tf.reduce_sum(pred_, 1), [bsize, 1]))
-    
-        hist_rater_a = tf.reduce_sum(pred_norm, 0)
-        hist_rater_b = tf.reduce_sum(y_true, 0)
-    
-        conf_mat = tf.matmul(tf.transpose(pred_norm), y_true)
-    
-        nom = tf.reduce_sum(weights * conf_mat)
-        denom = tf.reduce_sum(weights * tf.matmul(
-            tf.reshape(hist_rater_a, [N, 1]), tf.reshape(hist_rater_b, [1, N])) /
-                              tf.dtypes.cast(bsize), tf.float32)
-    
-        return nom / (denom + eps)
-
-
+from LossFunction import dice_coef_loss
 def unet(pretrained_weights = None, input_size = (512,512,1)):
     inputs = Input(input_size)
     conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
@@ -105,19 +42,10 @@ def unet(pretrained_weights = None, input_size = (512,512,1)):
     conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
 
     model = Model(inputs = inputs, outputs = conv10)
-
-    # Add custom metric here or balance classes.
-    # tf.keras.metrics.Recall()
-    # model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy', tf.keras.metrics.Recall()])
-    #model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = [tf.keras.metrics.Recall()])
     model.compile(optimizer = Adam(lr = 1e-4), loss = dice_coef_loss, metrics = ['accuracy', tf.keras.metrics.Recall()])
-
-    #model.compile(optimizer = Adam(lr = 1e-4), loss = kappa_loss, metrics = ['accuracy', tf.keras.metrics.Recall()])
     model.summary()
 
     if(pretrained_weights):
     	model.load_weights(pretrained_weights)
 
     return model
-
-
